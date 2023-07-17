d899a10f527a84b6ee71a77e50eab07c2859b7cb
“cristianosilvar” <cristianosilva2k@hotmail.com>
2023-07-17 11:33:42 -0300

    feat: created page not found

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
           <Routes>
             <Route path='/' element={<Home/>}/>
             <Route path='orders/:id' element={<Order/>} />
+            <Route path='*' element={<NotFound/>} />
           </Routes>
         </BrowserRouter>
       </div>
--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,8 +1,14 @@
 import style from './style/NotFound.module.css'
 
+import not_found from '../../assets/not_found.svg'
+
 export default function NotFound() {
     return (
-        <>
-        </>
+        <section>
+            <main className={style.not_found}>
+                <img src={not_found} alt="Imagem de unDraw" />
+                <h1 className='text-center fw-light'>A página não foi encontrada...</h1>
+            </main>
+        </section>
     )
 }
